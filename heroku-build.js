ccd977434b51d207be664d4d3b2c045dcb5475e3
Keisuke Kido <kskkido@gmail.com>
2017-12-23 19:11:04 +0900

    heroku build script

---
+++ heroku-build.js
@@ -0,0 +1,32 @@
+const proc = require('child_process')
+const { pkg } = require('./')
+
+if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')) {
+	const deps = pkg.devDependencies
+	let packages = ''
+
+	Object.keys(deps).forEach((key) => {
+		packages += `${key}@${deps[key]} ` // note space at end to separate entries
+	})
+
+	try {
+		console.time('install')
+		console.log('starting npm install of dev dependencies')
+		proc.execSync(`npm install ${packages}`)
+		console.timeEnd('install')
+
+		console.time('build')
+		console.log('starting npm build')
+		proc.execSync('npm run build:all')
+		console.timeEnd('build')
+
+		console.time('uninstall')
+		console.log('starting npm uninstall of dev dependencies')
+		proc.execSync(`npm uninstall ${packages}`)
+		console.timeEnd('uninstall')
+	} catch (err) {
+		console.error(err.message)
+	}
+} else {
+	console.log('Not Heroku, skipping postinstall build')
+}
