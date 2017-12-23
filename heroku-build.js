dcbdee4d67f6eb9801518500c47d42423219ed76
Keisuke Kido <kskkido@gmail.com>
2017-12-23 20:24:34 +0900

    fix build script

--- heroku-build.js
+++ heroku-build.js
@@ -12,12 +12,12 @@ if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/
 	try {
 		console.time('install')
 		console.log('starting npm install of dev dependencies')
-		proc.execSync(`npm install ${packages}`)
+		proc.execSync(`yarn add ${packages}`)
 		console.timeEnd('install')
 
 		console.time('build')
 		console.log('starting npm build')
-		proc.execSync('NODE_ENV=production npm run build:all')
+		proc.execSync('npm run build-prod')
 		console.timeEnd('build')
 
 		console.time('uninstall')
