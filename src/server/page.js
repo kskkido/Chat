93012692000c15a4c45cf87f90ff1bf52e49f173
Keisuke Kido <kskkido@gmail.com>
2017-12-24 17:00:57 +0900

    what

--- src/server/page.js
+++ src/server/page.js
@@ -8,7 +8,7 @@ const html = 	`
 		</head>
 		<body>
 			<div id="app"></div>
-			<script type="text/javascript" src="${fayeUrl}/client.js"></script>
+			<script type="text/javascript" src=${fayeUrl}/client.js></script>
 			<script type="text/javascript" src="dist/bundle.js"></script>
 		</body>
 	</html>
